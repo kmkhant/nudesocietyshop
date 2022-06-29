fdeac91e53702e63b9ebbf46421ea6667e954eb4
Khaing Myel Khant <khaingmyelkhant@ucsm.edu.mm>
2022-06-29 19:58:44 +0630

    update api

--- pages/api/update-algolia.ts
+++ pages/api/update-algolia.ts
@@ -4,6 +4,10 @@ import sanityClient, {
 	SanityDocumentStub,
 } from "sanity-algolia/node_modules/@sanity/client";
 import indexer from "sanity-algolia";
+import {
+	isValidSignature,
+	SIGNATURE_HEADER_NAME,
+} from "@sanity/webhook";
 
 const algolia = algoliasearch(
 	process.env.ALGOLIA_APPLICATION_ID ?? "",
@@ -18,13 +22,40 @@ const sanity = sanityClient({
 	useCdn: false,
 });
 
-export default function handler(
+const secret = process.env.ALGOLIA_ADMIN_API_KEY!;
+
+const readBody = async (readable: string) => {
+	const chunks = [];
+	for await (const chunk of readable) {
+		chunks.push(
+			typeof chunk === "string" ? Buffer.from(chunk) : chunk
+		);
+	}
+	return Buffer.concat(chunks).toString("utf-8");
+};
+
+export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
 	if (req.headers["content-type"] !== "application/json") {
 		res.status(400);
 		res.json({ message: "Bad Request" });
+		return;
+	}
+
+	const signature =
+		req.headers[SIGNATURE_HEADER_NAME] !== undefined
+			? req.headers[SIGNATURE_HEADER_NAME][0]
+			: "";
+
+	const body = await readBody(req.body);
+
+	if (!isValidSignature(body, signature, secret)) {
+		res.status(401).json({
+			success: false,
+			message: "Invalid Signature",
+		});
 		return;
 	}
 
